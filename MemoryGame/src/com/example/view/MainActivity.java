a8edbab2dffb856bc6141de1ed7f7c2dfc2f6129
William Holder <wdh1102@jagmail.southalabama.edu>
2013-04-20 13:38:00 -0500

    Removed three spaces below MainActivity
    Serious issues fixed

--- MemoryGame/src/com/example/view/MainActivity.java
+++ MemoryGame/src/com/example/view/MainActivity.java
@@ -15,9 +15,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.optionsui);
         loginSetup();
-        
-
-
     }
 
 
